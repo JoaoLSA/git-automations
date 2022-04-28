7affb847992d050bcb714d7295597c7a01355436
joaoalbuquerque <joao.albuquerque@teclat.com.br>
2022-04-28 08:33:19 -0300

    Add support for pushing to multiple branches at once

--- push.sh
+++ push.sh
@@ -1,26 +1,28 @@
-targetBranch=$1
+for var in "$@"
+do
+	if [ ! -z "$var" ] # check if branch name is present
+	then
+		targetBranch=$var
+		currentBranch=$(git symbolic-ref --short -q HEAD)
+		git add .
 
-if [ ! -z "$1" ] # check if the commit message is present
-then
-	currentBranch=$(git symbolic-ref --short -q HEAD)
-	git add .
+		git checkout $targetBranch
 
-	git checkout $targetBranch
+		git pull origin $targetBranch
 
-	git pull origin $targetBranch
+		git merge $currentBranch
 
-	git merge $currentBranch
+		if [ ! $? -eq 0 ]; then
+			echo "\nResolve conflicts and push..."
+		else
+			git commit -m "Merge with $currentBranch"
 
-	if [ ! $? -eq 0 ]; then
-		echo "\nResolve conflicts and push..."
+			git push origin $targetBranch
+			
+			git checkout $currentBranch
+		fi
 	else
-		git commit -m "Merge with $currentBranch"
-
-		git push origin $targetBranch
-		
-		git checkout $currentBranch
+		echo "InputError: Target branch was not provided"
 	fi
-else
-	echo "InputError: Target branch was not provided"
-fi
-	read # Make pause to read script output
+done
+read # Make pause to read script output
